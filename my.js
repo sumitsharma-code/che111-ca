60e9c1d13aaef6f7715ac8a12cbfa593df646cca
Sumitsha11 <97402908+sumitsharma-code@users.noreply.github.com>
2023-11-22 22:41:06 +0530

    ca of che111

---
+++ my.js
@@ -0,0 +1,7 @@
+var a = 1;
+// do {
+//     document.write(a);
+//     document.write("br");
+//     a++;
+// } while(a<=5);
+
---
+++ skjlg.py
@@ -0,0 +1,3 @@
+
+for i in range(1,13):
+    print(f'<option value="class{i}">{i}</option>')
